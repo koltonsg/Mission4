7803c42f099f3c3bec56c4e5404e1e2522eebaa0
koltonsg <153117250+koltonsg@users.noreply.github.com>
2025-01-27 12:44:14 -0700

    class and method names

--- Program.cs
+++ Program.cs
@@ -1,1 +1,4 @@
 ﻿// Group 4-8 | Kolton Gustin, Kaitlyn Fitzgerald, Becca Harrison, Jooyoung Jeon | Mission 4
+// Class Support
+// pubic PrintBoard(board) - prints the board to the console
+// public bool ValidWinner(board) - checks if there is a winner
